13fcdf72a66257839ae493b29798a4f6eb9b15b5
Monks09 <mayanksharma5104@gmail.com>
2022-12-02 02:51:55 +0530

    changes

--- scripts/wishlist.js
+++ scripts/wishlist.js
@@ -1,3 +1,13 @@
+import {header} from '../components/header.js';
+import {footer} from '../components/footer.js';
+
+let headerDiv = document.getElementById('header');
+headerDiv.innerHTML = header();
+
+let footerDiv = document.getElementById('footer');
+footerDiv.innerHTML = footer();
+
+
 let wishlistItems = JSON.parse(localStorage.getItem("wishlist"));
 console.log(wishlistItems);
 
