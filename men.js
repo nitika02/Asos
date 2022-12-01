4e316c68f1a3a14ca4d1d985d53f171c9a322378
Ni3deshwal <ni3deswal@gmail.com>
2022-12-01 16:31:13 +0530

    import

---
+++ men.js
@@ -0,0 +1,8 @@
+import {header} from "./components/header.js";
+import {footer} from "./components/footer.js";
+
+document.querySelector(".header").innerHTML=header();
+document.querySelector(".lfooter").innerHTML=footer();
+
+
+
