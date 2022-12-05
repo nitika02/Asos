cc5a01d7b1449a117dec2956c404d437a8178ae4
Monks09 <mayanksharma5104@gmail.com>
2022-12-05 15:15:07 +0530

    changes

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,11 +1,11 @@
-var product = JSON.parse(localStorage.getItem("cart")) || [];
-var items = document.getElementById("items");
-
+var product = JSON.parse(localStorage.getItem("cart"));
+var items = document.getElementById("item");
+// console.log(product);
 var total = localStorage.getItem('total');
-for (var i = 0; i < product.length; i++) {
+product.map( (el, i) => {
     var div = document.createElement("div");
     var img = document.createElement("img");
-    img.src = product[i].images[0];
+    img.src = el.images[0];
     var textDiv = document.createElement('div');
     textDiv.setAttribute('id', 'textDiv');
     var p = document.createElement("p");
@@ -17,30 +17,53 @@ for (var i = 0; i < product.length; i++) {
     deleteBut.addEventListener('click', () => {
         deleteElement(i);
     });
-    p.innerText = product[i].price;
+    p.innerText = el.price;
     // total = total + product[i].price
     p.style.fontWeight = "bold";
-    p1.innerText = product[i].title;
-    span1.innerText = product[i].color;
+    p1.innerText = el.title;
+    span1.innerText = el.color;
     smalldiv.append(span1);
     textDiv.append(p, p1, smalldiv, deleteBut);
     div.append(img, textDiv);
     items.append(div);
+    
 
-
-}
-document.getElementById('orderBtn').addEventListener('click', goToHome);
-function goToHome() {
+})
+document.getElementById('placeOrder').addEventListener('click', goToHome);
+// var confirmFun = confirm;
+function goToHome(){
     alert("Your order has Been placed");
-    window.location.href = "./index.html";
+   
+        window.location.href = "./index.html";
+   
 }
+var promo = document.getElementById("inputCode");
+var promoval = promo.value
 
-function deleteElement(i) {
+function deleteElement(i){
 
-    product.splice(i - 1, 1);
+    product.splice(i, 1);
     console.log(i);
     console.log(product);
     localStorage.setItem('cart', JSON.stringify(product));
     window.location.reload();
 }
 document.getElementById("totaltopay").innerText = total;
+
+document.getElementById("applyCode").addEventListener("click", applyPromo);
+function applyPromo(){
+    if(promo.value == "masai20"){
+        alert("Promo Code applied")
+        var promoTotal =  total;
+        promoTotal = promoTotal*20/100;
+        total = promoTotal;
+         document.getElementById("totaltopay").innerText = promoTotal;
+         console.log(promoTotal)
+        //  document.getElementById("subtotal").innerText = promoTotal;
+        //  window.location.reload();
+     }
+     else{
+        alert("Promo Code Invalid")
+     }
+    //  console.log(promo.value)
+}
